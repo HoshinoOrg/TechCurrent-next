822c48030582f974702b80c4c3b133284f20cef9
hirohoshi <114134734+hyrooon@users.noreply.github.com>
2024-08-29 22:28:28 +0900

    開発環境の構築 (#2)
    * 開発環境の構築
    
    * ESLintの設定

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// .eslintrc.js
+module.exports = {
+    extends: ['next', 'next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+    rules: {
+      // ここにプロジェクトに合わせたルールを追加できます
+    },
+  };
+  
