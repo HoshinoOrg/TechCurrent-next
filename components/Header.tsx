10611499fbeb59fbb1157117b8fed16c77081086
Hyrooon <h19970216h@gmail.com>
2024-09-16 19:26:32 +0900

    Lint対応

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,3 @@
-import NextLogo from "./NextLogo";
-import SupabaseLogo from "./SupabaseLogo";
-
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
@@ -12,8 +9,8 @@ export default function Header() {
         Tech Current is designed to help you master both the flow of technology
         and the current innovations shaping the industry. Our mission is to
         control the ever-changing currents of tech information and keep you
-        updated with the latest advancements. With Tech Current, you'll stay in
-        sync with both the latest trends and the immediate pulse of
+        updated with the latest advancements. With Tech Current, you will stay
+        in sync with both the latest trends and the immediate pulse of
         technological progress.
       </div>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
