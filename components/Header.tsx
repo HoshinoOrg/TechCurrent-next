346a026e72b0edcf861b8a690f468d39f878f9ae
hirohoshi <114134734+Hirohosi@users.noreply.github.com>
2024-09-29 21:26:47 +0900

    文言修正 (#26)

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        Teck Current
+        Tech Current
       </p>
       <div>
         {" "}
